45c657eb7101eb8e36814865499744a315e12dd9
Valmir Virtuoso <virtuoso751@gmail.com>
2024-12-01 17:57:41 -0300

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-function Home () {
-    return (
-        <div>
-            <h1>Meu site esta no ar!</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Meu site esta no ar!</h1>
+    </div>
+  );
 }
 
 export default Home;

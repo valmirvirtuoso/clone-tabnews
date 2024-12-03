09bcf05a23df7dff940847a1caedafb0482646d8
Valmir Virtuoso <virtuoso751@gmail.com>
2024-12-03 18:05:00 -0300

    adiciona imagem e novo texto

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h1>Meu site esta no ar!</h1>
-    </div>
-  );
-}
-
-export default Home;
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>Olha aqui embaixo um otario!!</h1>
+      <p>⬇️</p>
+      <img
+        src="https://scontent-gig4-1.xx.fbcdn.net/v/t39.30808-6/468396634_8799931746755829_488706238145590711_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=9g8fLUxctFIQ7kNvgGrIRc0&_nc_zt=23&_nc_ht=scontent-gig4-1.xx&_nc_gid=ALeyHHWdUIvtLCTG6K8QVmI&oh=00_AYAowPiFznqrXtt-EnCEePtYKbf4VFZtWvaSzEkC-DQU-g&oe=67555604"
+        alt="O gay"
+      />
+    </div>
+  );
+}
+
+export default Home;
